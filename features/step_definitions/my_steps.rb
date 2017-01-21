1f5725f6f6c55e4bd290fda9d14693ae48e69179
Igor Dorovskikh <i.dorovskikh@agilefusion.com>
2017-01-20 17:51:43 -0800

    adding Dropdown.feature with supported code

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -18,3 +18,20 @@ Then(/^I should see the "([^"]*)" message$/) do |message|
   puts "#{message}"
   expect(@browser.find_element(:css, '.example p').text).to eq(message)
 end
+
+Then(/^I move to "([^"]*)" page$/) do |page|
+  @browser.get "http://the-internet.herokuapp.com/#{page}"
+end
+
+When(/^I select "([^"]*)" from dropdown$/) do |option|
+  dropdown = @browser.find_element(id: 'dropdown')
+  @select_list = Selenium::WebDriver::Support::Select.new(dropdown)
+  @select_list.select_by(:text, option)
+end
+
+Then(/^I should see the "([^"]*)" selected$/) do |expected_selected_option|
+  actual_selected_option = @select_list.selected_options[0].text
+  expect(actual_selected_option).to eql expected_selected_option
+end
+
+
